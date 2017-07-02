584ca8ac13ac9ae8f0abf4949276f47c122080e4
yang-zhang <zhangyangcolostate@gmail.com>
2017-07-02 14:57:40 -0400

    Add r code

---
+++ stat/stat_tests_python_r.r
@@ -0,0 +1,15 @@
+?prop.test
+
+clicking_visitors <- c(526)
+all_visitors <- c(1000)
+prop.test(clicking_visitors, all_visitors)
+
+
+clicking_visitors <- c(435, 418)
+all_visitors <- c(1025, 998)
+prop.test(clicking_visitors, all_visitors)
+
+
+clicking_visitors <- c(435, 418, 412)
+all_visitors <- c(1025, 998, 990)
+prop.test(clicking_visitors, all_visitors)
