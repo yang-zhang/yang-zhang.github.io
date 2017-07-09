f7749d722169bff6497e57ca4730cce6d5d5558a
yang-zhang <zhangyangcolostate@gmail.com>
2017-07-09 14:43:14 -0400

    Update

--- ds_code/graph_demo.py
+++ ds_code/graph_demo.py
@@ -1,23 +1,27 @@
 
 # coding: utf-8
 
-# # Graph
+# # Graph functions in basic Python
+
+# Some basic graph functions using basic python.
 
 # In[1]:
 
 from inspect import getsourcelines
 
-import graph_func
-from importlib import reload
-reload(graph_func)
+import graph_func; from importlib import reload; reload(graph_func)
 from graph_func import nodes_from_network, connectivity, connections_of_each_node, connections_2nd_degree, print_source
 
+
+# Network: a list of tuples as edges of the network.
 
 # In[2]:
 
 network = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
          (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
 
+
+# ## Find all nodes in a network
 
 # In[3]:
 
@@ -30,6 +34,8 @@ nodes = nodes_from_network(network)
 nodes
 
 
+# ## Connectivity
+
 # In[5]:
 
 print_source(connectivity)
@@ -40,16 +46,30 @@ print_source(connectivity)
 connectivity(network)
 
 
+# ## Connections of each node
+
 # In[7]:
+
+print_source(connections_of_each_node)
+
+
+# In[8]:
 
 connections = connections_of_each_node(network)
 
 connections
 
 
+# ## 2nd-degree connections
+# 
 # Find 2nd-degree connections and number of common connections (friend of friend)
 
-# In[8]:
+# In[9]:
+
+print_source(connections_2nd_degree)
+
+
+# In[10]:
 
 for i in range(10):
     print(i, connections_2nd_degree(network, i))
--- ds_code/graph_func.py
+++ ds_code/graph_func.py
@@ -1,11 +1,17 @@
 from inspect import getsourcelines
 from collections import Counter
 
+
 def print_source(obj):
     print(''.join(getsourcelines(obj)[0]))
 
 
 def nodes_from_network(network):
+    """
+    Gets nodes from network edges.
+    :param network: a list of tuples as edges of the network.
+    :return: nodes of the network.
+    """
     nodes = set()
     for edge in network:
         nodes.add(edge[0])
@@ -14,6 +20,11 @@ def nodes_from_network(network):
 
 
 def connectivity(network):
+    """
+    Calculates connectivity of a network.
+    :param network: a list of tuples as edges of the network.
+    :return: connectivity of a network.
+    """
     nodes = nodes_from_network(network)
     number_nodes = len(nodes)
     number_edges = len(set(network))
@@ -22,6 +33,11 @@ def connectivity(network):
 
 
 def connections_of_each_node(network):
+    """
+    Find 1st-degree connections of each node in a network.
+    :param network: a list of tuples as edges of the network.
+    :return: 1st-degree connections of each node in a network.
+    """
     connections = dict()
     nodes = nodes_from_network(network)
     for node in nodes:
@@ -33,11 +49,16 @@ def connections_of_each_node(network):
 
 
 def connections_2nd_degree(network, node):
+    """
+    For the given node in a given network, finds each 2nd-degree connection and the number of shared nodes.
+    :param network: a list of tuples as edges of the network.
+    :param node: the node to find 2nd-degree connections for.
+    :return: a Counter object with 2nd-degree connection as the key and number of shared nodes as the value.
+    """
     connections = connections_of_each_node(network)
     connections_2nd = []
     for connection_1st in connections[node]:
         for connection_2nd in connections[connection_1st]:
-            if connection_2nd!=node and connection_2nd not in connections[node]:
+            if connection_2nd != node and connection_2nd not in connections[node]:
                 connections_2nd.append(connection_2nd)
-
     return Counter(connections_2nd)
