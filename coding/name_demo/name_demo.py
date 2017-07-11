52c97806120d875c5bb7b9c1b276d347eb6b3271
yang-zhang <zhangyangcolostate@gmail.com>
2017-07-10 22:37:20 -0400

    Add __name__ demo

---
+++ coding/name_demo/name_demo.py
@@ -0,0 +1,4 @@
+def say_name():
+	print(__name__)
+
+say_name()
---
+++ coding/name_demo/name_in_python.py
@@ -0,0 +1,45 @@
+
+# coding: utf-8
+
+# # What does `__name__` mean in Python?
+# 
+# `__name__` is `__main` when run as a script; `__name__` is name of the module when imported as a module.
+# Let's demo this using the script `name_demo.py`:
+
+# In[7]:
+
+cat name_demo.py
+
+
+# `__name__` is `__main` when run as a script:
+
+# In[8]:
+
+get_ipython().system('python name_demo.py')
+
+
+# `__name__` is name of the module (`name_demo`) when imported as a module:
+
+# In[9]:
+
+import name_demo
+
+
+# In[10]:
+
+name_demo.say_name()
+
+
+# `import ... as ...` does not change `__name__`:
+
+# In[11]:
+
+import name_demo as nd
+
+
+# In[12]:
+
+name_demo.say_name()
+
+
+# [Home](https://yang-zhang.github.io/)
