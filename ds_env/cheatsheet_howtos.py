8d4ea262b4d2a3500890f7f58a265881a6ee8133
yang-zhang <zhangyangcolostate@gmail.com>
2017-08-15 11:46:52 -0400

    Update

--- ds_env/cheatsheet_howtos.py
+++ ds_env/cheatsheet_howtos.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 # # Cheatsheet: howtos
-# 
+
 # ## Git
 # 
 # ### Add upstream repository
@@ -20,6 +20,61 @@
 # ```
 # conda create -n new_env --clone root
 # ```
+# 
+# ### Git setup for contributing to repo
+# - Fork the repo on github (e.g. https://github.com/dmlc/xgboost)
+# - Clone your forked repo: git clone https://github.com/yang-zhang/xgboost
+# - Add original owner’s repository: cd xgboost; git remote add dmlc https://github.com/dmlc/xgboost
+# - Show remote repo: git remote -v
+# See [here](http://kbroman.org/github_tutorial/pages/fork.html) for reference.
+# 
+# ### Add remote branch
+# ```
+# git checkout --track origin/name_of_the_remote_branch
+# ```
+
+# ## Docker
+# ### Get image and run
+# ```
+# docker pull julia
+# docker run -it --rm julia
+# ```
+# 
+# ### Get inside a running container
+# ```
+# docker ps
+# sudo docker exec -it gallant_fermat bash
+# ```
+# 
+# ### Get inside the most recent running container
+# ```
+# docker exec -it $(docker ps -l -q) bash
+# ```
+# 
+# ### Stop and delete all containers
+# ```
+# docker stop $(docker ps -a -q)
+# docker rm $(docker ps -a -q)
+# ```
+# 
+# ### Build image
+# ```
+# docker build --file dockerfiles/yang-zhang-ds.docker -t yang-zhang-ds .
+# ```
+# 
+# ### Map disk and run
+# ```
+# docker run -it --rm -v "$PWD":/usr/myapp -w /usr/myapp julia julia test.jl
+# ```
+# 
+# ### Append to env variables
+# ```
+# docker run --rm -v $PWD:/tmp -it yang-zhang-ds bash -c 'export PYTHONPATH=$PYTHONPATH:/tmp/ds-utils; bash' 
+# ```
+# 
+
+# 
+# ## Misc Hacks
 # 
 # ### Imports
 # Use a [customerized import script](https://github.com/yang-zhang/ds-utils/blob/master/ds_utils/imports.py) for frequent modules and setups. Add the following to the beginning of code:
@@ -39,20 +94,6 @@
 # 
 # Add [ds-utils](https://github.com/yang-zhang/ds-utils) to path:
 # 
-# 
-# ### Git setup for contributing to repo
-# - Fork the repo on github (e.g. https://github.com/dmlc/xgboost)
-# - Clone your forked repo: git clone https://github.com/yang-zhang/xgboost
-# - Add original owner’s repository: cd xgboost; git remote add dmlc https://github.com/dmlc/xgboost
-# - Show remote repo: git remote -v
-# See [here](http://kbroman.org/github_tutorial/pages/fork.html) for reference.
-# 
-# ### Add remote branch
-# ```
-# git checkout --track origin/name_of_the_remote_branch
-# ```
-# 
-# ## Misc Hacks
 # 
 # ### Python 2 to 3
 # ```
@@ -93,8 +134,3 @@
 # install.packages(c('entropy', 'psych', 'vcd'))
 # 
 # [Home](https://yang-zhang.github.io/)
-
-# In[ ]:
-
-
-
