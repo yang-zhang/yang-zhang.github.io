9806fed88eb5b74e2e469fbc568e6123f85ff57e
yang-zhang <zhangyangcolostate@gmail.com>
2017-08-27 21:35:31 -0400

    Update

--- ds_app/reddit/reddit_setup.py
+++ ds_app/reddit/reddit_setup.py
@@ -3,7 +3,7 @@
 
 # # Setup Reddit API
 
-# In[ ]:
+# In[1]:
 
 import pprint
 
@@ -22,20 +22,20 @@ from secrets import REDDIT_USER_NAME, REDDIT_PASSWORD, APP_CLIENT_ID, APP_CLIENT
 
 # ### Using requests
 
-# In[ ]:
+# In[2]:
 
 import requests
 
 
-# In[ ]:
+# In[3]:
 
 client_auth = requests.auth.HTTPBasicAuth(APP_CLIENT_ID, APP_CLIENT_SECRET)
 post_data = {"grant_type": "password", "username":REDDIT_USER_NAME, "password": REDDIT_PASSWORD}
-headers = {"User-Agent": "My Example App /0.1 by yang-zhang"}
+headers = {"User-Agent": "test:com.example.myredditapp:v0.0.0 (by /u/yang-zhang)"}
 response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
 
 
-# In[ ]:
+# In[4]:
 
 access_token = response.json()['access_token']
 
@@ -49,21 +49,21 @@ pprint.pprint(response.json())
 
 # ### Using praw
 
-# In[ ]:
+# In[6]:
 
 import praw
 reddit = praw.Reddit(client_id=APP_CLIENT_ID, client_secret=APP_CLIENT_SECRET,
-                     password=REDDIT_PASSWORD, user_agent="My Example App /0.1 by yang-zhang",
+                     password=REDDIT_PASSWORD, user_agent="test:com.example.myredditapp:v0.0.0 (by /u/yang-zhang)",
                      username=REDDIT_USER_NAME)
 
 
-# In[ ]:
+# In[7]:
 
 # Create a submission to /r/test
 reddit.subreddit('test').submit('Test Submission', url='https://reddit.com')
 
 
-# In[ ]:
+# In[8]:
 
 # Comment on a known submission
 submission = reddit.submission(url='https://www.reddit.com/comments/6vvlcm')
